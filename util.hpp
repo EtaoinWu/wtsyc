6dc743497bba2160afb10d647b02f9523ddb4478
Etaoin Wu <etaoin.wu@gmail.com>
2021-12-25 15:50:18 +0800

    add: c++ workaround utility

--- util.hpp
+++ util.hpp
@@ -1,6 +1,21 @@
 #pragma once
 
-namespace SysY::util {
-  template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
-  template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
-}
+namespace SysY {
+  template <class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+  template <class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+  template <class... Args> struct variant_cast_proxy {
+    std::variant<Args...> v;
+
+    template <class... ToArgs> operator std::variant<ToArgs...>() const {
+      return std::visit(
+          [](auto &&arg) -> std::variant<ToArgs...> { return arg; }, v);
+    }
+  };
+
+  template <class... Args>
+  auto variant_cast(const std::variant<Args...> &v)
+      -> variant_cast_proxy<Args...> {
+    return {v};
+  }
+} // namespace SysY
